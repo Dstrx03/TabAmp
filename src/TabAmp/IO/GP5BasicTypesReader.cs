e0c0fda7ee827d4d551b4be7a41384a41f96661c
Dstrx03 <dstrx03@gmail.com>
2022-11-01 17:13:51 +0200

    Add new API to GP5BasicTypesReader

--- src/TabAmp/IO/GP5BasicTypesReader.cs
+++ src/TabAmp/IO/GP5BasicTypesReader.cs
@@ -4,21 +4,65 @@ namespace TabAmp.IO;
 
 public class GP5BasicTypesReader
 {
-	private readonly ISequentialStreamReader _streamReader;
+    private readonly ISequentialStreamReader _streamReader;
 
-	public GP5BasicTypesReader(ISequentialStreamReader streamReader) =>
-		_streamReader = streamReader;
+    public GP5BasicTypesReader(ISequentialStreamReader streamReader) =>
+        _streamReader = streamReader;
 
-	public async Task<byte> ReadNextByteAsync()
-	{
-		var buffer = await _streamReader.ReadNextBytesAsync(1);
-		return buffer.Span[0];
-	}
+    public async Task<byte> ReadNextByteAsync()
+    {
+        var memory = await _streamReader.ReadNextBytesAsync(1);
+        return memory.Span[0];
+    }
 
-	public async Task<string> ReadNextByteSizeStringAsync()
-	{
-		var size = await ReadNextByteAsync();
-		var stringBytes = await _streamReader.ReadNextBytesAsync(size);
-		return Encoding.UTF8.GetString(stringBytes.Span);
-	}
+    public Task<sbyte> ReadNextSignedByteAsync() =>
+        ReadNextSignedByteAsync();
+
+    public Task<bool> ReadNextBoolAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<short> ReadNextShortAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> ReadNextIntAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<float> ReadNextFloatAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<double> ReadNextDoubleAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> ReadNextByteSizeStringAsync()
+    {
+        var size = await ReadNextByteAsync();
+        return await ReadNextStringAsync(size);
+    }
+
+    public async Task<string> ReadNextIntSizeStringAsync()
+    {
+        var size = await ReadNextIntAsync();
+        return await ReadNextStringAsync(size);
+    }
+
+    public Task<string> ReadNextIntByteSizeStringAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<string> ReadNextStringAsync(int size)
+    {
+        var memory = await _streamReader.ReadNextBytesAsync(size);
+        return Encoding.UTF8.GetString(memory.Span);
+    }
 }
