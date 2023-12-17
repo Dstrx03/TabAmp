1b7459cfd6e75e3c4677329ec9bf3af63745f482
Dstrx03 <dstrx03@gmail.com>
2023-12-17 16:44:27 +0200

    Add Cli Console app

---
+++ src/Cli.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+using TabAmp.Engine.Core.Score;
+
+namespace TabAmp.Cli.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var score = new Gp5Score();
+            System.Console.WriteLine("Hello, World!\n");
+            System.Console.WriteLine($"The Application agent\t{typeof(Program).FullName}");
+            System.Console.WriteLine($"The Score class\t\t{score.GetType().FullName}");
+        }
+    }
+}
