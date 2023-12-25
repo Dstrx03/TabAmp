df26c149751220b5f3500b418d1eff862b45a471
Dstrx03 <dstrx03@gmail.com>
2023-12-25 02:10:42 +0200

    Improve gp5 type decoders fluency

--- src/Engine.GuitarProFileFormat/Gp5ComplexTypesSerialDecoder.cs
+++ src/Engine.GuitarProFileFormat/Gp5ComplexTypesSerialDecoder.cs
@@ -2,47 +2,48 @@
 using System.Threading.Tasks;
 using TabAmp.Engine.GuitarProFileFormat.FileReader;
 
-namespace TabAmp.Engine.GuitarProFileFormat
+namespace TabAmp.Engine.GuitarProFileFormat;
+
+public class Gp5ComplexTypesSerialDecoder
 {
-    public class Gp5ComplexTypesSerialDecoder
+    private readonly ISerialAsynchronousFileReader _fileReader;
+    private readonly Gp5PrimitivesSerialDecoder _primitivesDecoder;
+
+    public Gp5ComplexTypesSerialDecoder(ISerialAsynchronousFileReader fileReader, Gp5PrimitivesSerialDecoder primitivesDecoder)
     {
-        private readonly ISerialAsynchronousFileReader _fileReader;
-        private readonly Gp5PrimitivesSerialDecoder _primitivesDecoder;
+        _fileReader = fileReader;
+        _primitivesDecoder = primitivesDecoder;
+    }
 
-        public Gp5ComplexTypesSerialDecoder(ISerialAsynchronousFileReader fileReader, Gp5PrimitivesSerialDecoder primitivesDecoder)
-        {
-            _fileReader = fileReader;
-            _primitivesDecoder = primitivesDecoder;
-        }
+    public async ValueTask<string> ReadStringAsync(int size)
+    {
+        var buffer = await _fileReader.ReadBytesAsync(size);
+        return Encoding.UTF8.GetString(buffer);
+    }
 
-        public async ValueTask<string> ReadByteSizeStringAsync(int readSize = 0)
-        {
-            var size = await _primitivesDecoder.ReadByteAsync();
-            return await ReadStringAsync(size, readSize);
-        }
+    public async ValueTask<string> ReadByteSizeStringAsync()
+    {
+        var size = await _primitivesDecoder.ReadByteAsync();
+        return await ReadStringAsync(size);
+    }
 
-        public async ValueTask<string> ReadIntSizeStringAsync(int readSize = 0)
-        {
-            var size = await _primitivesDecoder.ReadIntAsync();
-            return await ReadStringAsync(size, readSize);
-        }
+    public async ValueTask<string> ReadIntSizeStringAsync()
+    {
+        var size = await _primitivesDecoder.ReadIntAsync();
+        return await ReadStringAsync(size);
+    }
 
-        public async ValueTask<string> ReadIntByteSizeStringAsync()
-        {
-            var intValue = await _primitivesDecoder.ReadIntAsync();
-            var byteValue = await _primitivesDecoder.ReadByteAsync();
-            var readSize = intValue - 1;
-            var stringValue = await ReadStringAsync(byteValue, readSize);
-            return stringValue;
-        }
+    public async ValueTask<string> ReadIntByteSizeStringAsync()
+    {
+        var intValue = await _primitivesDecoder.ReadIntZeroBasedAsync();
+        var size = await _primitivesDecoder.ReadByteAsync();
 
-        private async ValueTask<string> ReadStringAsync(int size, int readSize)
-        {
-            var buffer = await _fileReader.ReadBytesAsync(size);
-            var skipBytes = readSize - size;
-            if (skipBytes > 0)
-                _fileReader.SkipBytes(skipBytes);
-            return Encoding.UTF8.GetString(buffer);
-        }
+        var stringValue = await ReadStringAsync(size);
+
+        var skipBytes = intValue - size;
+        if (skipBytes > 0)
+            _fileReader.SkipBytes(skipBytes);
+
+        return stringValue;
     }
 }
--- src/Engine.GuitarProFileFormat/Gp5PrimitivesSerialDecoder.cs
+++ src/Engine.GuitarProFileFormat/Gp5PrimitivesSerialDecoder.cs
@@ -2,65 +2,69 @@
 using System.Threading.Tasks;
 using TabAmp.Engine.GuitarProFileFormat.FileReader;
 
-namespace TabAmp.Engine.GuitarProFileFormat
+namespace TabAmp.Engine.GuitarProFileFormat;
+
+public class Gp5PrimitivesSerialDecoder
 {
-    public class Gp5PrimitivesSerialDecoder
+    public const int ByteSize = 1;
+    public const int SignedByteSize = 1;
+    public const int BoolSize = 1;
+    public const int ShortSize = 2;
+    public const int IntSize = 4;
+    public const int FloatSize = 4;
+    public const int DoubleSize = 8;
+
+    private readonly ISerialAsynchronousFileReader _fileReader;
+
+    public Gp5PrimitivesSerialDecoder(ISerialAsynchronousFileReader fileReader)
     {
-        public const int ByteSize = 1;
-        public const int SignedByteSize = 1;
-        public const int BoolSize = 1;
-        public const int ShortSize = 2;
-        public const int IntSize = 4;
-        public const int FloatSize = 4;
-        public const int DoubleSize = 8;
+        _fileReader = fileReader;
+    }
 
-        private readonly ISerialAsynchronousFileReader _fileReader;
+    public async ValueTask<byte> ReadByteAsync()
+    {
+        var buffer = await _fileReader.ReadBytesAsync(ByteSize);
+        return buffer[0];
+    }
 
-        public Gp5PrimitivesSerialDecoder(ISerialAsynchronousFileReader fileReader)
-        {
-            _fileReader = fileReader;
-        }
+    public async ValueTask<sbyte> ReadSignedByteAsync()
+    {
+        var buffer = await _fileReader.ReadBytesAsync(SignedByteSize);
+        return (sbyte)buffer[0];
+    }
 
-        public async ValueTask<byte> ReadByteAsync()
-        {
-            var buffer = await _fileReader.ReadBytesAsync(ByteSize);
-            return buffer[0];
-        }
+    public async ValueTask<bool> ReadBoolAsync()
+    {
+        var buffer = await _fileReader.ReadBytesAsync(ByteSize);
+        return buffer[0] == 1;
+    }
 
-        public async ValueTask<sbyte> ReadSignedByteAsync()
-        {
-            var buffer = await _fileReader.ReadBytesAsync(SignedByteSize);
-            return (sbyte)buffer[0];
-        }
+    public async ValueTask<short> ReadShortAsync()
+    {
+        var buffer = await _fileReader.ReadBytesAsync(ShortSize);
+        return BinaryPrimitives.ReadInt16LittleEndian(buffer);
+    }
 
-        public async ValueTask<bool> ReadBoolAsync()
-        {
-            var buffer = await _fileReader.ReadBytesAsync(ByteSize);
-            return buffer[0] == 1;
-        }
+    public async ValueTask<int> ReadIntAsync()
+    {
+        var buffer = await _fileReader.ReadBytesAsync(IntSize);
+        return BinaryPrimitives.ReadInt32LittleEndian(buffer);
+    }
 
-        public async ValueTask<short> ReadShortAsync()
-        {
-            var buffer = await _fileReader.ReadBytesAsync(ShortSize);
-            return BinaryPrimitives.ReadInt16LittleEndian(buffer);
-        }
+    public async ValueTask<int> ReadIntZeroBasedAsync()
+    {
+        return await ReadIntAsync() - 1;
+    }
 
-        public async ValueTask<int> ReadIntAsync()
-        {
-            var buffer = await _fileReader.ReadBytesAsync(IntSize);
-            return BinaryPrimitives.ReadInt32LittleEndian(buffer);
-        }
+    public async ValueTask<float> ReadFloatAsync()
+    {
+        var buffer = await _fileReader.ReadBytesAsync(FloatSize);
+        return BinaryPrimitives.ReadSingleLittleEndian(buffer);
+    }
 
-        public async ValueTask<float> ReadFloatAsync()
-        {
-            var buffer = await _fileReader.ReadBytesAsync(FloatSize);
-            return BinaryPrimitives.ReadSingleLittleEndian(buffer);
-        }
-
-        public async ValueTask<double> ReadDoubleAsync()
-        {
-            var buffer = await _fileReader.ReadBytesAsync(DoubleSize);
-            return BinaryPrimitives.ReadDoubleLittleEndian(buffer);
-        }
+    public async ValueTask<double> ReadDoubleAsync()
+    {
+        var buffer = await _fileReader.ReadBytesAsync(DoubleSize);
+        return BinaryPrimitives.ReadDoubleLittleEndian(buffer);
     }
 }
