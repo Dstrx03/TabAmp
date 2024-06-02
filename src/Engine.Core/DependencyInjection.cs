6370c4362b4876fe53d2cb1abba380b081ad46de
Dstrx03 <dstrx03@gmail.com>
2024-06-02 16:28:11 +0200

    Add main gp5 reader integrity validator

--- src/Engine.Core/DependencyInjection.cs
+++ src/Engine.Core/DependencyInjection.cs
@@ -18,9 +18,10 @@ public static class DependencyInjection
         services.AddScoped<IFileDeserializer<Gp5Score>, Gp5FileDeserializer>()
             .AddScoped<Gp5BinaryPrimitivesReader>()
             .AddScoped<Gp5StringsReader>()
+            .AddScoped<Gp5TodoReader>()
             .AddScoped<IGp5BinaryPrimitivesReader>(x => new Gp5BinaryPrimitivesReaderIntegrityValidator(x.GetRequiredService<Gp5BinaryPrimitivesReader>()))
             .AddScoped<IGp5StringsReader>(x => new Gp5StringsReaderIntegrityValidator(x.GetRequiredService<Gp5StringsReader>()))
-            .AddScoped<IGp5TodoReader, Gp5TodoReader>()
+            .AddScoped<IGp5TodoReader>(x => new Gp5TodoReaderIntegrityValidator(x.GetRequiredService<Gp5TodoReader>()))
             .AddScoped<IGp5RseEqualizerReader, Gp5RseEqualizerReader>();
 
         return services;
---
+++ src/Engine.Core/FileSerialization/GuitarPro/Gp5/Deserialization/Gp5TodoReaderIntegrityValidator.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+using TabAmp.Engine.Core.FileSerialization.Common.Exceptions;
+using TabAmp.Engine.Core.FileSerialization.GuitarPro.Gp5.Models;
+using TabAmp.Engine.Core.FileSerialization.GuitarPro.Gp5.Models.Strings;
+
+namespace TabAmp.Engine.Core.FileSerialization.GuitarPro.Gp5.Deserialization;
+
+internal class Gp5TodoReaderIntegrityValidator : IGp5TodoReader
+{
+    private readonly IGp5TodoReader _reader;
+
+    public Gp5TodoReaderIntegrityValidator(IGp5TodoReader reader) =>
+        _reader = reader;
+
+    public ValueTask<Gp5ByteString> ReadVersionAsync() =>
+        _reader.ReadVersionAsync();
+
+    public ValueTask<Gp5ScoreInformation> ReadScoreInformationAsync() =>
+        _reader.ReadScoreInformationAsync();
+
+    public ValueTask<Gp5Lyrics> ReadLyricsAsync() =>
+        _reader.ReadLyricsAsync();
+
+    public ValueTask<Gp5RseMasterEffect> ReadRseMasterEffectAsync() =>
+        _reader.ReadRseMasterEffectAsync();
+
+    public ValueTask<Gp5PageSetup> ReadPageSetupAsync() =>
+        _reader.ReadPageSetupAsync();
+
+    public ValueTask<Gp5Tempo> ReadHeaderTempoAsync() =>
+        _reader.ReadHeaderTempoAsync();
+
+    public ValueTask<Gp5HeaderKeySignature> ReadHeaderKeySignatureAsync() =>
+        _reader.ReadHeaderKeySignatureAsync();
+
+    public ValueTask<Gp5MidiChannel> ReadMidiChannelAsync() =>
+        _reader.ReadMidiChannelAsync();
+
+    public ValueTask<Gp5MusicalDirections> ReadMusicalDirectionsAsync() =>
+        _reader.ReadMusicalDirectionsAsync();
+
+    public ValueTask<int> ReadRseMasterEffectReverbAsync() =>
+        _reader.ReadRseMasterEffectReverbAsync();
+
+    public async ValueTask<int> ReadMeasuresCountAsync()
+    {
+        var measuresCount = await _reader.ReadMeasuresCountAsync();
+
+        if (measuresCount < 1 || measuresCount > 2048)
+            // TODO: message
+            throw new FileSerializationIntegrityException($"measuresCount out of valid range: measuresCount={measuresCount}");
+
+        return measuresCount;
+    }
+
+    public async ValueTask<int> ReadTracksCountAsync()
+    {
+        var tracksCount = await _reader.ReadTracksCountAsync();
+
+        if(tracksCount < 1 || tracksCount > 127)
+            // TODO: message
+            throw new FileSerializationIntegrityException($"tracksCount out of valid range: tracksCount={tracksCount}");
+
+        return tracksCount;
+    }
+
+    public ValueTask<Gp5MeasureHeader> ReadMeasureHeaderAsync(bool isFirst) =>
+        _reader.ReadMeasureHeaderAsync(isFirst);
+}
