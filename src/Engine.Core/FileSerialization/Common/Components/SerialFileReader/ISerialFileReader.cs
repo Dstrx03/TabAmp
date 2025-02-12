f19d8d31bdaf11492c0e0bbf565b7f55fd97214c
Dstrx03 <dstrx03@gmail.com>
2025-02-12 22:56:35 +0100

    Add array pool reference

--- src/Engine.Core/FileSerialization/Common/Components/SerialFileReader/ISerialFileReader.cs
+++ src/Engine.Core/FileSerialization/Common/Components/SerialFileReader/ISerialFileReader.cs
@@ -8,6 +8,8 @@ internal interface ISerialFileReader : IDisposable
     long Length { get; }
     long Position { get; }
 
+    delegate T TODO_Delegate_Name<T>(ReadOnlySpan<byte> buffer);
+
     ValueTask<byte[]> ReadBytesAsync(int count);
     ValueTask SkipBytesAsync(int count);
 }
