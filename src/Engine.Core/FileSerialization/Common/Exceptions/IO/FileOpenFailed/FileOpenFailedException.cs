da1654186601c866f2b8605ff2abe14e3fe9a907
Dstrx03 <dstrx03@gmail.com>
2025-05-11 23:23:24 +0200

    Implement FileOpenFailedException msg template

--- src/Engine.Core/FileSerialization/Common/Exceptions/IO/FileOpenFailed/FileOpenFailedException.cs
+++ src/Engine.Core/FileSerialization/Common/Exceptions/IO/FileOpenFailed/FileOpenFailedException.cs
@@ -4,11 +4,21 @@ namespace TabAmp.Engine.Core.FileSerialization.Common.Exceptions.IO.FileOpenFail
 
 internal sealed class FileOpenFailedException : FileSerializationException
 {
+    private const string MessageTemplate = "Could not open the file: {0}.";
+
     public FileOpenFailedException(Reason reason, Exception inner)
-        : base("", inner)
+        : base(ComposeMessage( reason ), inner)
     {
         Reason = reason;
     }
 
     public Reason Reason { get; }
+
+    private static string ComposeMessage(Reason reason) =>
+        string.Format(MessageTemplate, GetMessageComponent(reason));
+
+    private static string GetMessageComponent(Reason reason) => reason switch
+    {
+        _ => "TODO"
+    };
 }
