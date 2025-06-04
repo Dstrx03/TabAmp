d14d98c68accdff87ef4107884715883d2217316
Dstrx03 <dstrx03@gmail.com>
2025-06-05 01:42:32 +0200

    Move select operation stage to proper namespace

--- src/Engine.Core/FileSerialization/Common/Exceptions/IO/Operation/Fluent/OperationExceptionFluentBuilderSelectOperationStage.cs
+++ src/Engine.Core/FileSerialization/Common/Exceptions/IO/Operation/Fluent/OperationExceptionFluentBuilderSelectOperationStage.cs
@@ -1,20 +1,8 @@
-﻿using TabAmp.Engine.Core.FileSerialization.Common.Exceptions.IO.Operation.Fluent;
-
-namespace TabAmp.Engine.Core.FileSerialization.Common.Exceptions.IO.Operation;
+﻿namespace TabAmp.Engine.Core.FileSerialization.Common.Exceptions.IO.Operation.Fluent;
 
 internal abstract class OperationExceptionFluentBuilderSelectOperationStage<TException>
     where TException : OperationException
 {
     public static OperationExceptionFluentBuilder<TException> Read => new(Operation.Read);
     public static OperationExceptionFluentBuilder<TException> ReadSkip => new(Operation.ReadSkip);
-}
-
-internal partial class EndOfFileOperationException
-{
-    public abstract class With : OperationExceptionFluentBuilderSelectOperationStage<EndOfFileOperationException> { }
-}
-
-internal partial class NegativeBytesCountOperationException
-{
-    public abstract class With : OperationExceptionFluentBuilderSelectOperationStage<NegativeBytesCountOperationException> { }
 }
---
+++ src/Engine.Core/FileSerialization/Common/Exceptions/IO/Operation/Fluent/With.cs
@@ -0,0 +1,13 @@
+﻿using TabAmp.Engine.Core.FileSerialization.Common.Exceptions.IO.Operation.Fluent;
+
+namespace TabAmp.Engine.Core.FileSerialization.Common.Exceptions.IO.Operation;
+
+internal partial class EndOfFileOperationException
+{
+    public abstract class With : OperationExceptionFluentBuilderSelectOperationStage<EndOfFileOperationException> { }
+}
+
+internal partial class NegativeBytesCountOperationException
+{
+    public abstract class With : OperationExceptionFluentBuilderSelectOperationStage<NegativeBytesCountOperationException> { }
+}
