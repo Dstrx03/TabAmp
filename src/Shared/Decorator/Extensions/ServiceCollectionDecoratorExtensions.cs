517479d73b746a24060019d441549a70e534e189
Dstrx03 <dstrx03@gmail.com>
2025-08-17 01:10:26 +0200

    Add ServiceCollectionDecoratorExtensions

---
+++ src/Shared/Decorator/Extensions/ServiceCollectionDecoratorExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.Extensions.DependencyInjection.Decorator;
+
+public static class ServiceCollectionDecoratorExtensions
+{
+    public static IServiceCollection Todo(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection;
+    }
+}
