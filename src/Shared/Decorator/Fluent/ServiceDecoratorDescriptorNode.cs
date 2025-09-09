72895dba9a32cd3bd71e88164c809d7c59ae6c16
Dstrx03 <dstrx03@gmail.com>
2025-09-10 01:59:06 +0200

    Implement build descriptor chain

--- src/Shared/Decorator/Fluent/ServiceDecoratorDescriptorNode.cs
+++ src/Shared/Decorator/Fluent/ServiceDecoratorDescriptorNode.cs
@@ -1,5 +1,7 @@
 ﻿namespace TabAmp.Shared.Decorator.Fluent;
 
-public abstract record ServiceDecoratorDescriptorNode
+public abstract record ServiceDecoratorDescriptorNode(ServiceDecoratorDescriptorNode? Next, object _placeholder)
 {
+    internal sealed record Instance<TDecorator>(ServiceDecoratorDescriptorNode? Next) :
+        ServiceDecoratorDescriptorNode(Next, null);
 }
