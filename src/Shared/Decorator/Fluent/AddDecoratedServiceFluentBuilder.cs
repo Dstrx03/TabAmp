4431d8feda339d8ba0ad5d1c740aa4ed204baf61
Dstrx03 <dstrx03@gmail.com>
2025-09-10 15:16:00 +0200

    Make build descriptor chain method static

--- src/Shared/Decorator/Fluent/AddDecoratedServiceFluentBuilder.cs
+++ src/Shared/Decorator/Fluent/AddDecoratedServiceFluentBuilder.cs
@@ -2,15 +2,13 @@
 
 namespace TabAmp.Shared.Decorator.Fluent;
 
-public sealed class AddDecoratedServiceFluentBuilder<TService, TImplementation>(IServiceCollection todo) :
+public sealed class AddDecoratedServiceFluentBuilder<TService, TImplementation> :
     DecoratedServiceFluentBuilder<TService, TImplementation>
     where TService : class
     where TImplementation : class, TService
 {
     internal override IServiceCollection Scoped(ServiceDecoratorDescriptorNode<TService> descriptorChain)
     {
-        todo.AddScoped<TService>(serviceProvider =>
-            ComposeDecoratedService(serviceProvider, descriptorChain));
         throw new System.NotImplementedException();
     }
 }
