2568ac51fc8730f32ebe7dde077b0d11d5f4c7d3
Dstrx03 <dstrx03@gmail.com>
2025-10-28 17:56:48 +0100

    Add descriptor chain secondary public API

---
+++ src/Shared/Decorator/Extensions/ServiceDecoratorDescriptorChainFluentBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace TabAmp.Shared.Decorator.Fluent.Extensions;
+
+public static class ServiceDecoratorDescriptorChainFluentBuilderExtensions
+{
+    public static bool IsEmpty<TService>(this ServiceDecoratorDescriptorChainFluentBuilder<TService> builder)
+        where TService : notnull => builder.IsEmpty;
+
+    public static bool IsSingle<TService>(this ServiceDecoratorDescriptorChainFluentBuilder<TService> builder)
+        where TService : notnull => builder.IsSingle;
+
+    public static bool IsNormalized<TService>(this ServiceDecoratorDescriptorChainFluentBuilder<TService> builder)
+        where TService : notnull => builder.IsNormalized;
+}
