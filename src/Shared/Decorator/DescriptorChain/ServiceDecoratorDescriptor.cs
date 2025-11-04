fe6a0308d498240c03f6470cd68bab5dd9412240
Dstrx03 <dstrx03@gmail.com>
2025-11-04 01:27:57 +0100

    Fix decorator descriptor position name

--- src/Shared/Decorator/DescriptorChain/ServiceDecoratorDescriptor.cs
+++ src/Shared/Decorator/DescriptorChain/ServiceDecoratorDescriptor.cs
@@ -7,24 +7,24 @@ namespace TabAmp.Shared.Decorator.DescriptorChain;
 public abstract class ServiceDecoratorDescriptor<TService>
     where TService : notnull
 {
-    internal int? TODO_ID_NAME { get; private set; }
+    internal int? Position { get; private set; }
     internal ServiceDecoratorDescriptor<TService>? Next { get; private set; }
 
-    internal ServiceDecoratorDescriptor<TService> AppendTo(ServiceDecoratorDescriptor<TService> todo1)
+    internal ServiceDecoratorDescriptor<TService> AppendTo(ServiceDecoratorDescriptor<TService> descriptor)
     {
-        if (TODO_ID_NAME is not null)
-            throw new Exception($"TODO {nameof(AppendTo)} {nameof(TODO_ID_NAME)}:{TODO_ID_NAME}");
+        if (Position is not null)
+            throw new Exception($"TODO {nameof(AppendTo)} {nameof(Position)}:{Position}");
 
-        TODO_ID_NAME = todo1?.TODO_ID_NAME + 1 ?? 0;
-        Next = todo1;
+        Position = descriptor?.Position + 1 ?? 0;
+        Next = descriptor;
 
         return this;
     }
 
     internal void TODO_METHOD_NAME(ServiceDecoratorDescriptor<TService> todo2)
     {
-        if (TODO_ID_NAME is null)
-            throw new Exception($"TODO {nameof(TODO_METHOD_NAME)} {nameof(TODO_ID_NAME)}:{TODO_ID_NAME}");
+        if (Position is null)
+            throw new Exception($"TODO {nameof(TODO_METHOD_NAME)} {nameof(Position)}:{Position}");
 
         Next = todo2;
     }
