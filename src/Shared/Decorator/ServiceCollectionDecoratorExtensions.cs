069118479c6d56c15f4d48f3b290e4d007559677
Dstrx03 <dstrx03@gmail.com>
2025-12-08 00:58:46 +0100

    Extract core IServiceCollection extensions

--- src/Shared/Decorator/ServiceCollectionDecoratorExtensions.cs
+++ src/Shared/Decorator/ServiceCollectionDecoratorExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using TabAmp.Shared.Decorator.Core.Activators;
-using TabAmp.Shared.Decorator.Core.DescriptorChain;
+using TabAmp.Shared.Decorator.Core;
 using TabAmp.Shared.Decorator.Fluent;
 
 namespace TabAmp.Shared.Decorator;
@@ -255,60 +253,6 @@ public static class ServiceCollectionDecoratorExtensions
         serviceCollection.TryAddKeyedDecorated(serviceKey, builder, ServiceLifetime.Singleton);
     }
 
-    public static void AddDecorated<TService, TImplementation>(
-        this IServiceCollection serviceCollection,
-        ServiceDecoratorDescriptorChainFluentBuilder<TService, TImplementation> builder,
-        ServiceLifetime lifetime)
-        where TService : notnull
-        where TImplementation : notnull, TService
-    {
-        ArgumentNullException.ThrowIfNull(serviceCollection);
-
-        var descriptors = DescribeDecoratedService(builder, lifetime);
-        Add(serviceCollection, descriptors);
-    }
-
-    public static void AddKeyedDecorated<TService, TImplementation>(
-        this IServiceCollection serviceCollection,
-        object? serviceKey,
-        ServiceDecoratorDescriptorChainFluentBuilder<TService, TImplementation> builder,
-        ServiceLifetime lifetime)
-        where TService : notnull
-        where TImplementation : notnull, TService
-    {
-        ArgumentNullException.ThrowIfNull(serviceCollection);
-
-        var descriptors = DescribeKeyedDecoratedService(serviceKey, builder, lifetime);
-        Add(serviceCollection, descriptors);
-    }
-
-    public static void TryAddDecorated<TService, TImplementation>(
-        this IServiceCollection serviceCollection,
-        ServiceDecoratorDescriptorChainFluentBuilder<TService, TImplementation> builder,
-        ServiceLifetime lifetime)
-        where TService : notnull
-        where TImplementation : notnull, TService
-    {
-        ArgumentNullException.ThrowIfNull(serviceCollection);
-
-        var descriptors = DescribeDecoratedService(builder, lifetime);
-        TryAdd(serviceCollection, descriptors);
-    }
-
-    public static void TryAddKeyedDecorated<TService, TImplementation>(
-        this IServiceCollection serviceCollection,
-        object? serviceKey,
-        ServiceDecoratorDescriptorChainFluentBuilder<TService, TImplementation> builder,
-        ServiceLifetime lifetime)
-        where TService : notnull
-        where TImplementation : notnull, TService
-    {
-        ArgumentNullException.ThrowIfNull(serviceCollection);
-
-        var descriptors = DescribeKeyedDecoratedService(serviceKey, builder, lifetime);
-        TryAdd(serviceCollection, descriptors);
-    }
-
     public delegate ServiceDecoratorDescriptorChainFluentBuilder<TService, TImplementation> ConfigureDescriptorChain<TService, TImplementation>(
         ServiceDecoratorDescriptorChainFluentBuilder<TService, TImplementation> builder)
         where TService : notnull
@@ -323,100 +267,5 @@ public static class ServiceCollectionDecoratorExtensions
 
         var builder = TODO_NAME.For<TService, TImplementation>();
         return configureDescriptorChain(builder);
-    }
-
-    private static void Add(IServiceCollection serviceCollection, DecoratedServiceDescriptors descriptors)
-    {
-        var (implementationService, decoratedService) = descriptors;
-
-        if (implementationService is not null)
-            serviceCollection.Add(implementationService);
-
-        serviceCollection.Add(decoratedService);
-    }
-
-    private static void TryAdd(IServiceCollection serviceCollection, DecoratedServiceDescriptors descriptors)
-    {
-        var (implementationService, decoratedService) = descriptors;
-
-        if (implementationService is not null)
-            serviceCollection.TryAdd(implementationService);
-
-        serviceCollection.TryAdd(decoratedService);
-    }
-
-    private static DecoratedServiceDescriptors DescribeDecoratedService<TService, TImplementation>(
-        ServiceDecoratorDescriptorChainFluentBuilder<TService, TImplementation> builder,
-        ServiceLifetime lifetime)
-        where TService : notnull
-        where TImplementation : notnull, TService
-    {
-        var descriptorChain = builder.BuildDescriptorChain();
-
-        var implementationServiceDescriptor =
-            DescribeImplementationService<TService, TImplementation>(descriptorChain, lifetime);
-
-        var decoratedServiceDescriptor = ServiceDescriptor.Describe(
-            serviceType: typeof(TService),
-            implementationFactory: serviceProvider =>
-                DecoratedServiceActivator.CreateService<TService, TImplementation>(serviceProvider, descriptorChain),
-            lifetime: lifetime);
-
-        return new(implementationService: implementationServiceDescriptor,
-            decoratedService: decoratedServiceDescriptor);
-    }
-
-    private static DecoratedServiceDescriptors DescribeKeyedDecoratedService<TService, TImplementation>(
-        object? serviceKey,
-        ServiceDecoratorDescriptorChainFluentBuilder<TService, TImplementation> builder,
-        ServiceLifetime lifetime)
-        where TService : notnull
-        where TImplementation : notnull, TService
-    {
-        var descriptorChain = builder.BuildDescriptorChain();
-
-        var implementationServiceDescriptor =
-            DescribeImplementationService<TService, TImplementation>(descriptorChain, lifetime);
-
-        var decoratedServiceDescriptor = ServiceDescriptor.DescribeKeyed(
-            serviceType: typeof(TService),
-            serviceKey: serviceKey,
-            implementationFactory: (serviceProvider, _) =>
-                DecoratedServiceActivator.CreateService<TService, TImplementation>(serviceProvider, descriptorChain),
-            lifetime: lifetime);
-
-        return new(implementationService: implementationServiceDescriptor,
-            decoratedService: decoratedServiceDescriptor);
-    }
-
-    private static ServiceDescriptor? DescribeImplementationService<TService, TImplementation>(
-        ServiceDecoratorDescriptorChain<TService> descriptorChain,
-        ServiceLifetime lifetime)
-        where TService : notnull
-        where TImplementation : notnull, TService
-    {
-        if (!descriptorChain.TODO_NAME)
-            return null;
-
-        var implementationType = typeof(TImplementation);
-        return ServiceDescriptor.DescribeKeyed(
-            serviceType: implementationType,
-            serviceKey: descriptorChain,
-            implementationType: implementationType,
-            lifetime: lifetime);
-    }
-
-    private readonly ref struct DecoratedServiceDescriptors(
-        ServiceDescriptor? implementationService,
-        ServiceDescriptor decoratedService)
-    {
-        internal ServiceDescriptor? ImplementationService { get; } = implementationService;
-        internal ServiceDescriptor DecoratedService { get; } = decoratedService;
-
-        public void Deconstruct(out ServiceDescriptor? implementationService, out ServiceDescriptor decoratedService)
-        {
-            implementationService = ImplementationService;
-            decoratedService = DecoratedService;
-        }
     }
 }
