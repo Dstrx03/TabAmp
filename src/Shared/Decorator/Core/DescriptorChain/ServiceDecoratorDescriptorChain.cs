081ba52ca66b98faa226e47bbd053bf428ecf810
Dstrx03 <dstrx03@gmail.com>
2025-12-15 00:09:54 +0100

    Detect disposable service decorator

--- src/Shared/Decorator/Core/DescriptorChain/ServiceDecoratorDescriptorChain.cs
+++ src/Shared/Decorator/Core/DescriptorChain/ServiceDecoratorDescriptorChain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using TabAmp.Shared.Decorator.Core.Activators;
+using TabAmp.Shared.Decorator.Core.Extensions;
 
 namespace TabAmp.Shared.Decorator.Core.DescriptorChain;
 
@@ -8,15 +9,23 @@ internal abstract class ServiceDecoratorDescriptorChain<TService>
 {
     internal ServiceDecoratorDescriptorChain<TService>? Next { get; }
 
-    private ServiceDecoratorDescriptorChain(ServiceDecoratorDescriptorChain<TService>? next) =>
+    internal bool IsDisposable { get; }
+    internal bool IsAsyncDisposable { get; }
+
+    private ServiceDecoratorDescriptorChain(ServiceDecoratorDescriptorChain<TService>? next, Type decoratorType)
+    {
         Next = next;
+
+        IsDisposable = decoratorType.IsDisposable();
+        IsAsyncDisposable = decoratorType.IsAsyncDisposable();
+    }
 
     internal IServiceDecoratorDescriptorChainMetadata? Metadata => this as IServiceDecoratorDescriptorChainMetadata;
 
     internal abstract TService CreateDecorator(IServiceProvider serviceProvider, TService service);
 
     internal sealed class Node<TDecorator>(ServiceDecoratorDescriptorChain<TService>? next) :
-        ServiceDecoratorDescriptorChain<TService>(next)
+        ServiceDecoratorDescriptorChain<TService>(next, typeof(TDecorator))
         where TDecorator : notnull, TService
     {
         internal override TService CreateDecorator(IServiceProvider serviceProvider, TService service) =>
@@ -26,7 +35,7 @@ internal abstract class ServiceDecoratorDescriptorChain<TService>
     internal sealed class MetadataNode<TDecorator>(
         ServiceDecoratorDescriptorChain<TService>? next,
         object? implementationServiceKey) :
-        ServiceDecoratorDescriptorChain<TService>(next),
+        ServiceDecoratorDescriptorChain<TService>(next, typeof(TDecorator)),
         IServiceDecoratorDescriptorChainMetadata
         where TDecorator : notnull, TService
     {
