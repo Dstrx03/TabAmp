6d2862ddcfc9db3768a087c9608aedfbc3205be3
Dstrx03 <dstrx03@gmail.com>
2025-12-19 01:38:58 +0100

    Implement POC disposing decorator

--- src/Shared/Decorator/Core/Activators/DecoratedServiceActivator.cs
+++ src/Shared/Decorator/Core/Activators/DecoratedServiceActivator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
+using TabAmp.Shared.Decorator.Core.Decorators;
 using TabAmp.Shared.Decorator.Core.DescriptorChain;
 
 namespace TabAmp.Shared.Decorator.Core.Activators;
@@ -16,12 +17,17 @@ internal static class DecoratedServiceActivator
         ArgumentNullException.ThrowIfNull(descriptorChain);
 
         var service = GetImplementationService<TService, TImplementation>(serviceProvider, descriptorChain);
+        var a = GetA(descriptorChain);
         var descriptor = descriptorChain;
         while (descriptor is not null)
         {
             service = descriptor.CreateDecorator(serviceProvider, service);
+            a?.TODO1(service, descriptor);
             descriptor = descriptor.Next;
         }
+
+        if (a is not null)
+            service = a.TODO2(service);
 
         return service;
     }
@@ -36,5 +42,14 @@ internal static class DecoratedServiceActivator
             return ActivatorUtilities.CreateInstance<TImplementation>(serviceProvider);
 
         return serviceProvider.GetRequiredKeyedService<TImplementation>(serviceKey: descriptorChain.ImplementationServiceKey);
+    }
+
+    private static A<TService>? GetA<TService>(ServiceDecoratorDescriptorChain<TService> descriptorChain)
+        where TService : notnull
+    {
+        if (!descriptorChain.UseA)
+            return null;
+
+        return new();
     }
 }
---
+++ src/Shared/Decorator/Core/Decorators/A.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TabAmp.Shared.Decorator.Core.DescriptorChain;
+
+namespace TabAmp.Shared.Decorator.Core.Decorators;
+
+internal class A<TService>
+    where TService : notnull
+{
+    internal void TODO1(TService service, ServiceDecoratorDescriptorChain<TService> descriptor)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal TService TODO2(TService service)
+    {
+        throw new NotImplementedException();
+    }
+}
