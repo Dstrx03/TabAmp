7f1def0b6aeea43eafdc26cbe625bfb7b191e8ca
Dstrx03 <dstrx03@gmail.com>
2026-02-01 23:50:20 +0100

    Extract descriptor chain validation methods

--- src/Shared/Decorator/Core/DescriptorChain/Validation/ServiceDecoratorDescriptorChainValidator.cs
+++ src/Shared/Decorator/Core/DescriptorChain/Validation/ServiceDecoratorDescriptorChainValidator.cs
@@ -15,6 +15,13 @@ internal static class ServiceDecoratorDescriptorChainValidator
     {
         ArgumentNullException.ThrowIfNull(descriptorChain);
         List<Exception>? errors = null;
+        if (HasDisposableContainer(descriptorChain) && !descriptorChain.IsDisposableContainerAllowed) AddError(ref errors, new InvalidOperationException("TODO: ..."));
+        return new(errors);
+    }
+
+    private static bool HasDisposableContainer<TService>(ServiceDecoratorDescriptorChain<TService> descriptorChain)
+        where TService : class
+    {
         var hasDisposableContainer = false;
         var descriptor = descriptorChain;
         while (descriptor is not null)
@@ -24,11 +31,12 @@ internal static class ServiceDecoratorDescriptorChainValidator
             if (isDisposable && (isInner || hasDisposableContainer)) hasDisposableContainer = true;
             descriptor = descriptor.Next;
         }
-        if (hasDisposableContainer && !descriptorChain.IsDisposableContainerAllowed)
-        {
-            errors ??= [];
-            errors.Add(new InvalidOperationException("TODO: ..."));
-        }
-        return new(errors);
+        return hasDisposableContainer;
+    }
+
+    private static void AddError(ref List<Exception>? errors, Exception error)
+    {
+        errors ??= [];
+        errors.Add(error);
     }
 }
