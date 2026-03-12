d550cadb21cb424b787bb8d6916def230d37fda0
Dstrx03 <dstrx03@gmail.com>
2026-03-13 00:02:06 +0100

    Proceed with API design

--- src/Shared/Decorator/Core/Activators/CtorHelper.cs
+++ src/Shared/Decorator/Core/Activators/CtorHelper.cs
@@ -41,23 +41,44 @@ public static class CtorHelper
                 return input;
         }
 
+        if (D(input).FooBar(ref input))
+            return input;
+
         return input;
     }
 
-    public static int GetD() => D();
-
-    private static int D()
+    public static int GetD()
     {
-        return 12345;
+        var output = D(new());
+        throw new NotImplementedException();
+    }
+
+    private static Output<int> D(Input input)
+    {
+        if (true)
+        {
+            var error = new InvalidOperationException("D error...");
+            if (error.FooBar(ref input))
+                return input.ToOutput<int>();
+        }
+
+        return input.ToOutput(12345);
     }
 
     public readonly ref struct Input
     {
+        public Output<T> ToOutput<T>() => new();
+        public Output<T> ToOutput<T>(T value) => new();
         public static implicit operator Output(Input input) => new();
         public static implicit operator ServiceDecoratorDescriptorChainValidationResult(Input input) => new();
     }
 
     public readonly ref struct Output
+    {
+        public bool FooBar(ref Input input) => false;
+    }
+
+    public readonly ref struct Output<T>
     {
         public bool FooBar(ref Input input) => false;
     }
