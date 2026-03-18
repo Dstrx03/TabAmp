5b40d6592d43c7046ca950584edb6c6abaed44f2
Dstrx03 <dstrx03@gmail.com>
2026-03-19 00:54:02 +0100

    Add ErrorsCollection

---
+++ src/Shared/Validation/ErrorsCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TabAmp.Shared.Validation;
+
+internal readonly ref struct ErrorsCollection
+{
+    private readonly object? _errors;
+    private readonly int _length;
+
+    private ErrorsCollection(object? errors, int length) => (_errors, _length) = (errors, length);
+
+    internal ErrorsCollection Add(Exception error)
+    {
+        object? errors = null;
+        int length = 0;
+
+        throw new NotImplementedException();
+        return new(errors, length);
+    }
+}
