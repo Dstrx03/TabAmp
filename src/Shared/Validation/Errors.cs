964f0eb605d2ce448d190b497ecf3b6c066286b8
Dstrx03 <dstrx03@gmail.com>
2026-03-19 23:39:24 +0100

    Add Add implementation change

---
+++ src/Shared/Validation/Errors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TabAmp.Shared.Validation;
+
+internal readonly ref struct Errors
+{
+    private readonly object? _errors;
+    private readonly int _length;
+
+    private Errors(object? errors, int length) => (_errors, _length) = (errors, length);
+
+    private bool IsEmpty => _length == 0;
+    private bool IsSingle => _length == 1;
+    private bool IsMany => _length > 1;
+
+    private Errors Add(Exception error)
+    {
+        ArgumentNullException.ThrowIfNull(error);
+        object? errors = null;
+        var length = _length + 1;
+        return new(errors, length);
+    }
+
+    private static Errors Empty => new();
+}
