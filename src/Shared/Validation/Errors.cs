14f05071dd15cde66c548a35429012c824d89187
Dstrx03 <dstrx03@gmail.com>
2026-03-20 22:21:01 +0100

    Add Add implementation completion

--- src/Shared/Validation/Errors.cs
+++ src/Shared/Validation/Errors.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace TabAmp.Shared.Validation;
 
@@ -7,7 +9,8 @@ internal readonly ref struct Errors
     private readonly object? _errors;
     private readonly int _length;
 
-    private Errors(object? errors, int length) => (_errors, _length) = (errors, length);
+    private Errors(object? errors, int length) =>
+        (_errors, _length) = (errors, length);
 
     private bool IsEmpty => _length == 0;
     private bool IsSingle => _length == 1;
@@ -16,9 +19,27 @@ internal readonly ref struct Errors
     private Errors Add(Exception error)
     {
         ArgumentNullException.ThrowIfNull(error);
-        object? errors = null;
-        var length = _length + 1;
-        return new(errors, length);
+
+        object errors = this switch
+        {
+            _ when IsEmpty => error,
+            _ when IsSingle => AddAsSingle(error),
+            _ when IsMany => AddAsMany(error),
+            _ => throw new UnreachableException()
+        };
+
+        return new(errors, _length + 1);
+    }
+
+    private List<Exception> AddAsSingle(Exception error) =>
+        [(_errors as Exception)!, error];
+
+    private List<Exception> AddAsMany(Exception error)
+    {
+        var errors = (_errors as List<Exception>)!;
+        errors.Add(error);
+
+        return errors;
     }
 
     private static Errors Empty => new();
