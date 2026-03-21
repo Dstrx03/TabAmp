cfb72e8381bdb281db19091e5a091836ea23e202
Dstrx03 <dstrx03@gmail.com>
2026-03-21 02:35:22 +0100

    Wipe playground

--- src/Cli.Console/Tests.cs
+++ src/Cli.Console/Tests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using TabAmp.Shared.Validation;
-using TabAmp.Shared.Validation.Extensions;
 
 namespace TabAmp.Cli.Console;
 
@@ -8,49 +7,6 @@ internal static class Tests
 {
     public static ValidationResult SomeValidationMethod(bool stopOnFirstError = false)
     {
-        var scope = Scope.Init_TODONAME(stopOnFirstError);
-
-        if (SomeValidationMethodA(scope).WithError_TODONAME(ref scope))
-            return scope;
-
-        if (true)
-        {
-            var error = new InvalidOperationException("Some error...");
-            if (error.WithError_TODONAME(ref scope))
-                return scope;
-        }
-
-        return scope;
-    }
-
-    private static ScopeResult SomeValidationMethodA(Context context)
-    {
-        var scope = context.ToScope();
-
-        if (SomeValidationMethodB(scope).WithError_TODONAME(ref scope))
-            return scope;
-
-        if (true)
-        {
-            var error = new InvalidOperationException("A error...");
-            if (error.WithError_TODONAME(ref scope))
-                return scope;
-        }
-
-        return scope;
-    }
-
-    private static ScopeResult SomeValidationMethodB(Context context)
-    {
-        var scope = context.ToScope();
-
-        if (true)
-        {
-            var error = new InvalidOperationException("B error...");
-            if (error.WithError_TODONAME(ref scope))
-                return scope;
-        }
-
-        return scope;
+        throw new NotImplementedException();
     }
 }
