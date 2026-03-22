acce3d886f6c56fccd2ca1e31a41ce919d065fa4
Dstrx03 <dstrx03@gmail.com>
2026-03-22 23:09:06 +0100

    Implement Enumerator

--- src/Shared/Validation/Errors.cs
+++ src/Shared/Validation/Errors.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 
@@ -7,15 +6,24 @@ namespace TabAmp.Shared.Validation;
 
 internal readonly ref struct Errors
 {
-    private readonly object? _errors;
+    private readonly object? _storage;
     private readonly int _length;
 
-    private Errors(object? errors, int length) =>
-        (_errors, _length) = (errors, length);
+    public Errors() => throw ThrowHelper.TODO(typeof(Errors));
 
-    private bool IsEmpty => _length == 0;
+    private Errors(object? storage, int length) =>
+        (_storage, _length) = (storage, length);
+
+    public int Count => _length;
+
+    public bool Any => _length > 0;
+    public bool IsEmpty => _length == 0;
+
     private bool IsSingle => _length == 1;
     private bool IsMany => _length > 1;
+
+    private Exception AsSingle => (_storage as Exception)!;
+    private List<Exception> AsMany => (_storage as List<Exception>)!;
 
     private Errors Add(Exception error)
     {
@@ -23,57 +31,53 @@ internal readonly ref struct Errors
 
         object errors = this switch
         {
-            _ when IsEmpty => error,
-            _ when IsSingle => AddAsSingle(error),
-            _ when IsMany => AddAsMany(error),
+            { IsEmpty: true } => error,
+            { IsSingle: true } => AddAsSingle(error),
+            { IsMany: true } => AddAsMany(error),
             _ => throw new UnreachableException()
         };
 
         return new(errors, _length + 1);
     }
 
-    private List<Exception> AddAsSingle(Exception error) =>
-        [(_errors as Exception)!, error];
+    private List<Exception> AddAsSingle(Exception error) => [AsSingle, error];
 
     private List<Exception> AddAsMany(Exception error)
     {
-        var errors = (_errors as List<Exception>)!;
-        errors.Add(error);
+        var storage = AsMany;
+        storage.Add(error);
 
-        return errors;
+        return storage;
     }
 
     public Enumerator GetEnumerator() => new();
 
     private static Errors Empty => new();
 
-    public ref struct Enumerator : IEnumerator<Exception>
+    public ref struct Enumerator
     {
         private readonly Errors _errors;
-        private int _index;
+        private int _index = -1;
+
+        public Enumerator() => throw ThrowHelper.TODO(typeof(Enumerator));
+
         private Enumerator(Errors errors) => _errors = errors;
+
         public readonly Exception Current => _errors switch
         {
-            { IsSingle: true } => (_errors._errors as Exception)!,
-            { IsMany: true } => (_errors._errors as List<Exception>)![_index],
+            { IsSingle: true } => _errors.AsSingle,
+            { IsMany: true } => _errors.AsMany[_index],
             _ => throw new UnreachableException()
         };
-        readonly object IEnumerator.Current => Current;
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
+
         public bool MoveNext()
         {
             if (_errors.IsEmpty)
                 return false;
 
-            return _index++ == 0;
-            throw new NotImplementedException();
+            return ++_index < _errors._length;
         }
-        public void Reset()
-        {
-            throw new NotImplementedException();
-        }
+
+        private static Enumerator FromErrors(Errors errors) => new(errors);
     }
 }
---
+++ src/Shared/Validation/ThrowHelper.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TabAmp.Shared.Validation;
+
+internal static class ThrowHelper
+{
+    internal static InvalidOperationException TODO(Type type) => new("Public ctor is not allowed TODO... - " + type.Name);
+}
