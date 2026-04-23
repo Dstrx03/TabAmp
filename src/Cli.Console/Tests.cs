f9a28e658f32da5f723a8327887deac45adcc5a8
Dstrx03 <dstrx03@gmail.com>
2026-04-24 00:50:07 +0200

    Expand tests

--- src/Cli.Console/Tests.cs
+++ src/Cli.Console/Tests.cs
@@ -9,20 +9,46 @@ internal static class Tests
     public static void Run()
     {
         var result = SomeValidationMethod();
+        var err = result.Errors.ToList();
     }
 
     private static ValidationResult SomeValidationMethod(Scope scope = default)
     {
         new InvalidOperationException("some error").CaptureBy(ref scope);
+        var err1 = scope.ToResult().Errors.ToList();
 
         var result = InnerValidationMethod(scope.ToInner());
         result.CaptureBy(ref scope);
+        var err2 = result.Errors.ToList();
+
+        var noErrorsResult = NoErrorsValidationMethod(scope.ToInner());
+        noErrorsResult.CaptureBy(ref scope);
+        var err3 = noErrorsResult.Errors.ToList();
+
+        var doubleErrorsResult = DoubleErrorsValidationMethod(scope.ToInner());
+        doubleErrorsResult.CaptureBy(ref scope);
+        var err4 = doubleErrorsResult.Errors.ToList();
 
         return scope.ToResult();
     }
 
     private static ValidationResult InnerValidationMethod(Scope scope = default)
     {
+        new InvalidOperationException("some inner error").CaptureBy(ref scope);
+
+        return scope.ToResult();
+    }
+
+    private static ValidationResult NoErrorsValidationMethod(Scope scope = default)
+    {
+        return scope.ToResult();
+    }
+
+    private static ValidationResult DoubleErrorsValidationMethod(Scope scope = default)
+    {
+        new InvalidOperationException("some double error A").CaptureBy(ref scope);
+        new InvalidOperationException("some double error B").CaptureBy(ref scope);
+
         return scope.ToResult();
     }
 }
