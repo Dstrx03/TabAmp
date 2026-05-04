368fa9dc528a40cad025e3a64ca52e35cacf27d5
Dstrx03 <dstrx03@gmail.com>
2026-05-05 00:55:59 +0200

    Implement arbitrary errors cases tests

--- src/Cli.Console/Tests.cs
+++ src/Cli.Console/Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using TabAmp.Shared.Validation;
 using TabAmp.Shared.Validation.Extensions;
 
@@ -6,117 +7,66 @@ namespace TabAmp.Cli.Console;
 
 internal static class Tests
 {
+    private static readonly HashSet<string> _errors = [
+        "SomeValidationMethod_A",
+        "SomeInnerValidationMethod_A",
+        "SomeInnerInnerValidationMethod_A",
+        "SomeInnerInnerValidationMethod_B"
+    ];
+
     public static void Run()
     {
         var result = SomeValidationMethod();
-        var err = result.Errors.ToList();
     }
 
     private static ValidationResult SomeValidationMethod(Scope scope = default)
     {
-        new InvalidOperationException("some error").CaptureBy(ref scope);
-        var err1 = scope.ToResult().Errors.ToList();
+        if (HasError("SomeValidationMethod_A"))
+        {
+            var error = new InvalidOperationException("SomeValidationMethod error A.");
+            if (error.CaptureBy(ref scope).ShouldStop)
+                return scope.ToResult();
+        }
 
-        var result = InnerValidationMethod(scope.ToInner());
-        result.CaptureBy(ref scope);
-        var err2 = result.Errors.ToList();
-
-        var valueResult = ValueValidationMethod(scope.ToInner());
-        valueResult.CaptureBy(ref scope);
-        var value3 = valueResult.Value;
-        var err3 = valueResult.Errors.ToList();
-
-        var noErrorsResult = NoErrorsValidationMethod(scope.ToInner());
-        noErrorsResult.CaptureBy(ref scope);
-        var err4 = noErrorsResult.Errors.ToList();
-
-        var valueWithErrorResult = ValueWithErrorValidationMethod(scope.ToInner());
-        valueWithErrorResult.CaptureBy(ref scope);
-        var value5 = valueWithErrorResult.Value;
-        var err5 = valueWithErrorResult.Errors.ToList();
-
-        var nullableValueWithErrorResult = NullableValueWithErrorValidationMethod(scope.ToInner());
-        nullableValueWithErrorResult.CaptureBy(ref scope);
-        var value6 = nullableValueWithErrorResult.Value;
-        var err6 = nullableValueWithErrorResult.Errors.ToList();
-
-        var stringResult = StringValidationMethod(scope.ToInner());
-        stringResult.CaptureBy(ref scope);
-        var value7 = stringResult.Value;
-        var err7 = stringResult.Errors.ToList();
-
-        var stringWithErrorResult = StringWithErrorValidationMethod(scope.ToInner());
-        stringWithErrorResult.CaptureBy(ref scope);
-        var value8 = stringWithErrorResult.Value;
-        var err8 = stringWithErrorResult.Errors.ToList();
-
-        var nullableStringWithErrorResult = NullableStringWithErrorValidationMethod(scope.ToInner());
-        nullableStringWithErrorResult.CaptureBy(ref scope);
-        var value9 = nullableStringWithErrorResult.Value;
-        var err9 = nullableStringWithErrorResult.Errors.ToList();
-
-        var doubleErrorsResult = DoubleErrorsValidationMethod(scope.ToInner());
-        doubleErrorsResult.CaptureBy(ref scope);
-        var err10 = doubleErrorsResult.Errors.ToList();
+        if (SomeInnerValidationMethod(scope.ToInner()).CaptureBy(ref scope).ShouldStop)
+            return scope.ToResult();
 
         return scope.ToResult();
     }
 
-    private static ValidationResult InnerValidationMethod(Scope scope = default)
+    private static ValidationResult SomeInnerValidationMethod(Scope scope = default)
     {
-        new InvalidOperationException("some inner error").CaptureBy(ref scope);
+        if (HasError("SomeInnerValidationMethod_A"))
+        {
+            var error = new InvalidOperationException("SomeInnerValidationMethod error A.");
+            if (error.CaptureBy(ref scope).ShouldStop)
+                return scope.ToResult();
+        }
+
+        if (SomeInnerInnerValidationMethod(scope.ToInner()).CaptureBy(ref scope).ShouldStop)
+            return scope.ToResult();
 
         return scope.ToResult();
     }
 
-    private static ValidationResult NoErrorsValidationMethod(Scope scope = default)
+    private static ValidationResult SomeInnerInnerValidationMethod(Scope scope = default)
     {
+        if (HasError("SomeInnerInnerValidationMethod_A"))
+        {
+            var error = new InvalidOperationException("SomeInnerInnerValidationMethod error A.");
+            if (error.CaptureBy(ref scope).ShouldStop)
+                return scope.ToResult();
+        }
+
+        if (HasError("SomeInnerInnerValidationMethod_B"))
+        {
+            var error = new InvalidOperationException("SomeInnerInnerValidationMethod error B.");
+            if (error.CaptureBy(ref scope).ShouldStop)
+                return scope.ToResult();
+        }
+
         return scope.ToResult();
     }
 
-    private static ValidationResult DoubleErrorsValidationMethod(Scope scope = default)
-    {
-        new InvalidOperationException("some double error A").CaptureBy(ref scope);
-        new InvalidOperationException("some double error B").CaptureBy(ref scope);
-
-        return scope.ToResult();
-    }
-
-    private static ValidationResult<int> ValueValidationMethod(Scope scope = default)
-    {
-        return scope.ToResult(12345);
-    }
-
-    private static ValidationResult<int> ValueWithErrorValidationMethod(Scope scope = default)
-    {
-        new InvalidOperationException("some value error").CaptureBy(ref scope);
-
-        return scope.ToResult<int>();
-    }
-
-    private static ValidationResult<int?> NullableValueWithErrorValidationMethod(Scope scope = default)
-    {
-        new InvalidOperationException("some nullable value error").CaptureBy(ref scope);
-
-        return scope.ToResult<int?>();
-    }
-
-    private static ValidationResult<string> StringValidationMethod(Scope scope = default)
-    {
-        return scope.ToResult("I am a result.");
-    }
-
-    private static ValidationResult<string> StringWithErrorValidationMethod(Scope scope = default)
-    {
-        new InvalidOperationException("some string error").CaptureBy(ref scope);
-
-        return scope.ToResult<string>();
-    }
-
-    private static ValidationResult<string?> NullableStringWithErrorValidationMethod(Scope scope = default)
-    {
-        new InvalidOperationException("some nullable string error").CaptureBy(ref scope);
-
-        return scope.ToResult<string?>();
-    }
+    private static bool HasError(string error) => _errors.Contains(error);
 }
