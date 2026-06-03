bb4762858e93b81982519521d7c0aa03052a63b2
Dstrx03 <dstrx03@gmail.com>
2026-06-04 00:40:22 +0200

    Extract ServiceDecoratorConstructorDiscovery

--- src/Shared/Decorator/Core/Activators/ServiceDecoratorActivator.cs
+++ src/Shared/Decorator/Core/Activators/ServiceDecoratorActivator.cs
@@ -22,33 +22,7 @@ internal static class ServiceDecoratorActivator
     private static ConstructorInfo ResolveDecoratorConstructorInfo<TService, TDecorator>()
         where TDecorator : TService
     {
-        return DiscoverDecoratorConstructorInfo<TService, TDecorator>();
-    }
-
-    private static ConstructorInfo DiscoverDecoratorConstructorInfo<TService, TDecorator>()
-        where TDecorator : TService
-    {
-        var serviceType = typeof(TService);
-        var decoratorType = typeof(TDecorator);
-
-        ConstructorInfo? constructorInfo = null;
-        foreach (var constructor in decoratorType.GetConstructors())
-        {
-            foreach (var parameter in constructor.GetParameters())
-            {
-                if (parameter.ParameterType != serviceType)
-                    continue;
-
-                if (constructorInfo == constructor)
-                    throw MultipleServiceTypeParametersDecoratorConstructorException(serviceType, decoratorType, constructorInfo);
-                else if (constructorInfo is not null)
-                    throw AmbiguousDecoratorConstructorException(decoratorType, constructorInfo, constructor);
-
-                constructorInfo = constructor;
-            }
-        }
-
-        return constructorInfo ?? throw MissingDecoratorConstructorException(serviceType, decoratorType);
+        return ServiceDecoratorConstructorDiscovery.DiscoverConstructor<TService, TDecorator>();
     }
 
     private static object[] ResolveDecoratorConstructorParameters<TService>(
@@ -69,25 +43,4 @@ internal static class ServiceDecoratorActivator
 
         return parameters;
     }
-
-    private static InvalidOperationException MultipleServiceTypeParametersDecoratorConstructorException(
-        Type serviceType,
-        Type decoratorType,
-        ConstructorInfo constructorInfo) =>
-        new($"Unable to activate decorator type '{decoratorType.FullName}'. " +
-            $"Constructor has multiple parameters of the decorated type '{serviceType.FullName}':{Environment.NewLine}" +
-            $"{constructorInfo}");
-
-    private static InvalidOperationException AmbiguousDecoratorConstructorException(
-        Type decoratorType,
-        ConstructorInfo constructorInfo,
-        ConstructorInfo constructorInfoOther) =>
-        new($"Unable to activate decorator type '{decoratorType.FullName}'. " +
-            $"The following constructors are ambiguous:{Environment.NewLine}" +
-            $"{constructorInfo}{Environment.NewLine}" +
-            $"{constructorInfoOther}");
-
-    private static InvalidOperationException MissingDecoratorConstructorException(Type serviceType, Type decoratorType) =>
-        new($"Unable to activate decorator type '{decoratorType.FullName}'. " +
-            $"Missing constructor with a parameter for the decorated type '{serviceType.FullName}'.");
 }
---
+++ src/Shared/Decorator/Core/Activators/ServiceDecoratorConstructorDiscovery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+
+namespace TabAmp.Shared.Decorator.Core.Activators;
+
+internal static class ServiceDecoratorConstructorDiscovery
+{
+    internal static ConstructorInfo DiscoverConstructor<TService, TDecorator>()
+        where TDecorator : TService
+    {
+        var serviceType = typeof(TService);
+        var decoratorType = typeof(TDecorator);
+
+        ConstructorInfo? constructorInfo = null;
+        foreach (var constructor in decoratorType.GetConstructors())
+        {
+            foreach (var parameter in constructor.GetParameters())
+            {
+                if (parameter.ParameterType != serviceType)
+                    continue;
+
+                if (constructorInfo == constructor)
+                    throw MultipleServiceTypeParametersDecoratorConstructorException(serviceType, decoratorType, constructorInfo);
+                else if (constructorInfo is not null)
+                    throw AmbiguousDecoratorConstructorException(decoratorType, constructorInfo, constructor);
+
+                constructorInfo = constructor;
+            }
+        }
+
+        return constructorInfo ?? throw MissingDecoratorConstructorException(serviceType, decoratorType);
+    }
+
+    private static InvalidOperationException MultipleServiceTypeParametersDecoratorConstructorException(
+        Type serviceType,
+        Type decoratorType,
+        ConstructorInfo constructorInfo) =>
+        new($"Unable to activate decorator type '{decoratorType.FullName}'. " +
+            $"Constructor has multiple parameters of the decorated type '{serviceType.FullName}':{Environment.NewLine}" +
+            $"{constructorInfo}");
+
+    private static InvalidOperationException AmbiguousDecoratorConstructorException(
+        Type decoratorType,
+        ConstructorInfo constructorInfo,
+        ConstructorInfo constructorInfoOther) =>
+        new($"Unable to activate decorator type '{decoratorType.FullName}'. " +
+            $"The following constructors are ambiguous:{Environment.NewLine}" +
+            $"{constructorInfo}{Environment.NewLine}" +
+            $"{constructorInfoOther}");
+
+    private static InvalidOperationException MissingDecoratorConstructorException(Type serviceType, Type decoratorType) =>
+        new($"Unable to activate decorator type '{decoratorType.FullName}'. " +
+            $"Missing constructor with a parameter for the decorated type '{serviceType.FullName}'.");
+}
